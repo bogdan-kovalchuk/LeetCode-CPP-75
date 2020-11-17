9be5160e295948fb5e3614c6cd67f4d7fd376495
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-11-17 16:38:35 +0200

    Find the Town Judge

--- 997. Find the Town Judge/Find_the_town_judge.cpp
+++ 997. Find the Town Judge/Find_the_town_judge.cpp
@@ -3,13 +3,23 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findJudge(int N, vector<vector<int>>& trust) {
-        
+    int findJudge(int N, vector<vector<int>> &trust)
+    {
+        return 1; 
     }
 };
 
-int main() {
+int main()
+{
 
+    int N = 4;
+    vector<vector<int>> trust = {{1, 3}, {1, 4}, {2, 3}, {2, 4}, {4, 3}};
+
+    Solution solution;
+    cout << solution.findJudge(N, trust);
+
+    return 0;
 }
