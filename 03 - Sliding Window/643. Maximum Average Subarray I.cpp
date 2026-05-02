22740a6bbccd24b36cc1cbae5f64d0eefc6b4261
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-05-02 16:15:52 +0800

    643 solution

--- 03 - Sliding Window/643. Maximum Average Subarray I.cpp
+++ 03 - Sliding Window/643. Maximum Average Subarray I.cpp
@@ -1,15 +1,28 @@
 #include "../leetcode_test.hpp"
+#include <numeric>
 
-class Solution {
+class Solution
+{
 public:
-    double findMaxAverage(vector<int>& nums, int k) {
-        throw logic_error("Not implemented");
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        int sum = std::accumulate(nums.begin(), nums.begin() + k, 0);
+        int max_sum = sum;
+
+        for (int i = k; i < static_cast<int>(nums.size()); ++i)
+        {
+            sum = sum - nums[i - k] + nums[i];
+            max_sum = std::max(max_sum, sum);
+        }
+
+        return static_cast<double>(max_sum) / k;
     }
 };
 
 // LOCAL_TEST_BEGIN
 #ifdef LOCAL_TEST
-int main() {
+int main()
+{
     {
         Solution solution;
         vector<int> nums = vector<int>{1, 12, -5, -6, 50, 3};
