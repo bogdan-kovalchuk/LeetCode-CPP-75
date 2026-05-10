db9d63da481d3f14013b0bb2465871a162c94dbc
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-05-10 10:55:48 +0300

    1657 solution

--- 05 - Hash Map and Set/1657. Determine if Two Strings Are Close.cpp
+++ 05 - Hash Map and Set/1657. Determine if Two Strings Are Close.cpp
@@ -1,15 +1,49 @@
 #include "../leetcode_test.hpp"
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
 
-class Solution {
+class Solution
+{
 public:
-    bool closeStrings(string word1, string word2) {
-        throw logic_error("Not implemented");
+    bool closeStrings(string word1, string word2)
+    {
+        if (word1.length() != word2.length())
+            return false;
+
+        unordered_set<char> word1Chars(word1.begin(), word1.end());
+        unordered_set<char> word2Chars(word2.begin(), word2.end());
+
+        if (word1Chars != word2Chars)
+            return false;
+
+        unordered_map<char, int> word1Map;
+        for (const auto &ch : word1)
+            word1Map[ch]++;
+
+        unordered_map<char, int> word2Map;
+        for (const auto &ch : word2)
+            word2Map[ch]++;
+
+        vector<int> word1Freq;
+        for (auto &[k, v] : word1Map)
+            word1Freq.push_back(v);
+
+        vector<int> word2Freq;
+        for (auto &[k, v] : word2Map)
+            word2Freq.push_back(v);
+
+        sort(word1Freq.begin(), word1Freq.end());
+        sort(word2Freq.begin(), word2Freq.end());
+
+        return word1Freq == word2Freq;
     }
 };
 
 // LOCAL_TEST_BEGIN
 #ifdef LOCAL_TEST
-int main() {
+int main()
+{
     {
         Solution solution;
         string word1 = "abc";
